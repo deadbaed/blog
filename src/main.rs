0997c83114616af194e817b5ed83a50c14d16ad4
Philippe Loctaux <p@philippeloctaux.com>
2025-09-07 01:30:02 +0200

    debug: auto reload html page if content changes

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,113 @@ fn main() {
 
     let content = leptos_ssg::Content::scan_path(&content_path).unwrap();
 
+    #[cfg(debug_assertions)]
+    fn debug_auto_reload() -> leptos::prelude::AnyView {
+        use leptos::prelude::*;
+
+        view! {
+        <script
+        data-interval="2500"
+        data-debug
+        inner_html=r#"
+// https://github.com/Kalabasa/simple-live-reload/blob/bb65d8b3f19af8c6477cdefb0c718c1db3d0cb69/script.js
+
+/*Copyright 2025 Lean Rada.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the “Software”), to deal in the Software without restriction, including
+without limitation therights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+if (
+  location.hostname === "localhost" ||
+  location.hostname === "127.0.0.1" ||
+  location.hostname === "[::1]"
+) {
+  const interval = Number(document.currentScript?.dataset.interval || 1000);
+  const debug = document.currentScript?.hasAttribute("data-debug") || false;
+
+  let watching = new Set();
+  watch(location.href);
+
+  new PerformanceObserver((list) => {
+    for (const entry of list.getEntries()) {
+      watch(entry.name);
+    }
+  }).observe({ type: "resource", buffered: true });
+
+  function watch(urlString) {
+    if (!urlString) return;
+    const url = new URL(urlString);
+    if (url.origin !== location.origin) return;
+
+    if (watching.has(url.href)) return;
+    watching.add(url.href);
+
+    if (debug) {
+      console.log("[simple-live-reload] watching", url.href);
+    }
+
+    let focused = false;
+    let etag, lastModified, contentLength;
+    let request = { method: "head", cache: "no-store" };
+
+    async function check() {
+      try {
+        if (document.hidden) return;
+        if (focused) return;
+      } finally {
+        focused = document.hasFocus();
+      }
+
+      const res = await fetch(url, request);
+      if (res.status === 405 || res.status === 501) {
+        request.method = "get";
+        request.headers = {
+          Range: "bytes=0-0",
+        };
+        return check();
+      }
+
+      const newETag = res.headers.get("ETag");
+      const newLastModified = res.headers.get("Last-Modified");
+      const newContentLength = res.headers.get("Content-Length");
+
+      if (
+        (etag && etag !== newETag) ||
+        (lastModified && lastModified !== newLastModified) ||
+        (contentLength && contentLength !== newContentLength)
+      ) {
+        if (debug) {
+          console.log("[simple-live-reload] change detected in", url.href);
+        }
+        try {
+          location.reload();
+        } catch (e) {
+          location = location;
+        }
+      }
+
+      etag = newETag;
+      lastModified = newLastModified;
+      contentLength = newContentLength;
+    }
+
+    check();
+    setInterval(check, interval);
+    document.addEventListener(
+      "visibilitychange",
+      () => !document.hidden && check()
+    );
+  }
+}"#></script>
+        }.into_any()
+    }
+
     fn additional_js() -> Option<leptos::prelude::AnyView> {
         use leptos::prelude::*;
 
@@ -61,6 +168,10 @@ fn main() {
             };
         "#></script>
             <script data-goatcounter="https://goatcounter.philt3r.eu/count" async src="https://goatcounter.philt3r.eu/count.js"></script>
+            {
+              #[cfg(debug_assertions)]
+              debug_auto_reload()
+            }
         };
         Some(additional_js.into_any())
     }
